e8d66b37c33c837baae0e22e333c34e53cade954
Valen <Epggservicios>
2026-06-18 09:51:29 -0300

    tati

---
+++ index2.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const conectarDB = () => {
+
+const mongoURI = process.env.MONGO_URI
+mongoose.connect(mongoURI) 
+   .then(() => { console.log('conexion exitosa')
+})
+   .catch((error) => {
+    console.error('error al conectar a mongoDB:', error);
+    process.exit(1); 
+   });
+};
+module.exports = conectarDB 
