32c94f52cc9f676387c6ecc3fe848354195c1693
Valen <Epggservicios>
2026-06-19 15:47:25 -0300

    cosa

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const conectarDB = () => {
 
-const mongoURI = process.env.MONGO_URI
+const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI) 
    .then(() => { console.log('conexion exitosa')
 })
