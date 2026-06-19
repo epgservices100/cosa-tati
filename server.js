90c2b7f3f8b33c8e6ca424b6e6564c096299d1b0
Valen <Epggservicios>
2026-06-19 15:58:04 -0300

    cosa

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ const PORT = process.env.PORT || 10000;
 // Conectar a la base de datos
 conectarDB();
 
-// Middleware para procesar solicitudes JSON
-app.use(express.json());
+
+
 
 // 1. Servir los archivos estáticos de la interfaz desde la carpeta 'public'
-app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.static(path.join(__dirname)));
+app.use(express.json());
 
-// 2. Ruta comodín configurada con la sintaxis obligatoria de Express v5.
-// Esto intercepta cualquier petición de navegación y le entrega el archivo index.html
-app.get('/{*splat}', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Public', 'index.html'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Iniciar el servidor
